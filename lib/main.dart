a1fc607bd547ffd48bfbd93c6ebe7f06be507974
Jean de Dieu <amourhouegbelo@gmail.com>
2020-08-01 14:59:50 +0100

    garde moi les codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:premierchoixapp/Authentification/connexion.dart';
 import 'package:premierchoixapp/Authentification/inscription.dart';
-import 'package:premierchoixapp/Composants/hexadecimal.dart';
 import 'package:premierchoixapp/Design/Article.dart';
-import 'package:premierchoixapp/Design/FirstPage.dart';
-import 'Authentification/Decision.dart';
+import 'package:premierchoixapp/Design/Decision.dart';
+import 'package:premierchoixapp/Design/Favoris1.dart';
+import 'package:premierchoixapp/Design/Notification2.dart';
+import 'package:premierchoixapp/Design/Page1.dart';
+import 'package:premierchoixapp/test.dart';
 import 'Authentification/connexion.dart';
-import 'Authentification/renisialisation_passwd.dart';
-import 'Authentification/renseignements.dart';
-import 'Composants/hexadecimal.dart';
-import 'Navigations_pages/Pages_article_paniers/Panier1.dart';
-import 'Navigations_pages/all_navigation_page.dart';
-import 'Navigations_pages/panier.dart';
-import 'Pages/page_chargement.dart';
+import 'Design/Notification1.dart';
+import 'Design/Panier.dart';
+import 'Design/Panier2.dart';
+import 'Design/Panier3.dart';
+
+
+
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
+
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: HexColor("#001C36"),
-        brightness: Brightness.light,
-        accentColor: HexColor("#001C36"),
-        splashColor: HexColor("#001C36"),
-        colorScheme: ColorScheme.light(primary: HexColor("#001C36")),
-      ),
+      theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-      initialRoute: FirstPage.id,
+      initialRoute: Panier3.id,
       routes: {
-        Connexion.id: (context) => Connexion(),
+        Connexion.id:(context) => Connexion(),
         Inscription.id: (context) => Inscription(),
-        Article.id: (context) => Article(),
-        Panier.id: (context) => Panier(),
-        Panier1.id: (context) => Panier1(),
-        ResetPasswd.id: (context) => ResetPasswd(),
-        Renseignements.id: (context) => Renseignements(),
-        AllNavigationPage.id: (context) => AllNavigationPage(),
-        PageChargement.id: (context) => PageChargement(),
-        Decision.id: (context) => Decision(),
-        FirstPage.id: (context) => FirstPage(),
+        Page1.id: (context) => Page1(),
+        Article.id:(context) => Article(),
+        Panier.id:(context) => Panier(),
+        Panier2.id:(context) => Panier2(),
+        Panier3.id:(context) => Panier3(),
+        Favoris1.id:(context) => Favoris1(),
+        Notification2.id:(context) => Notification2(),
+        Notification1.id:(context) => Notification1(),
+        Decision.id:(context) => Decision(),
+
       },
     );
   }
 }
+
