75a4d0ae5683dd9565e7947f99169f4d1b44c2d3
Jean de Dieu <amourhouegbelo@gmail.com>
2020-10-29 13:45:22 +0100

    Add animation in begin Page

--- lib/Navigations_pages/accueil.dart
+++ lib/Navigations_pages/accueil.dart
@@ -102,7 +102,7 @@ class _AccueilState extends State<Accueil> with SingleTickerProviderStateMixin {
                 }
             ),
             toAnimate: true,
-            position: BadgePosition(top: 0, right: 0),
+            position: BadgePosition(top: 0, end: 0),
             child: IconButton(
                 icon: Icon(
                   Icons.local_grocery_store,
