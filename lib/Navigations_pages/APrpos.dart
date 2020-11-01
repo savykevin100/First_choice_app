bbd3c12b96c0811509758ee4a80b1aed5c0799f9
Jean de Dieu <amourhouegbelo@gmail.com>
2020-11-01 18:55:23 +0100

    Adding Apropos and ConditionsGenerales pages

--- lib/Navigations_pages/APrpos.dart
+++ lib/Navigations_pages/APrpos.dart
@@ -14,6 +14,8 @@ class APrpos extends StatefulWidget {
 class _APrposState extends State<APrpos> {
   ScrollController controller=ScrollController();
   int ajoutPanier;
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     AppBarClasse _appBar = AppBarClasse(
@@ -23,15 +25,255 @@ class _APrposState extends State<APrpos> {
         );
     return Scaffold(
       backgroundColor: HexColor("#F5F5F5"),
-      appBar: _appBar.appBarFunctionStream(),
+      appBar: AppBar(
+        title: Text(
+          "A Propos",
+          style: TextStyle(color: Colors.white, fontFamily: "MonseraBold",fontSize: 18),
+        ),
+      ),
+      key: _scaffoldKey,
       body: new SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: longueurPerCent(20.0, context),),
             Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Text("Lorem Ipsum"),
-            )
+              child: Text("AVANT-PROPOS",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 16,
+                  fontFamily: "MonseraBold",
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text("Le présent support fournit les Mentions Légales, Conditions Générales d’Utilisation et Conditions Générales de Vente liées à l’utilisation de la plateforme 1er CHOIX conçue, développée et exploitée par FOLLOW ME, entreprise individuelle de droit béninois. Tout utilisateur du support déployé notamment sous la forme d’une application mobile, est invité à prendre connaissance desdits supports légaux qui régissent les rapports entre les différents exploitants de 1er CHOIX.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 15,
+                  fontFamily: "Monsera_Light",
+                ),
+              ),
+            ),
+            SizedBox(height: longueurPerCent(50.0, context),),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("MENTIONS LEGALES",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: HexColor("#001C36"),
+                    fontSize: 17,
+                    fontFamily: "MonseraBold",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: longueurPerCent(10.0, context),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("AVERTISSEMENT",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 16,
+                  fontFamily: "MonseraBold",
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text("Merci de lire intégralement et attentivement les présentes conditions d’utilisation et d’exploitation avant d’accéder à l’application mobile 1er CHOIX. En vous connectant, vous reconnaissez en avoir pris connaissance, les accepter sans réserve et vous engagez à vous conformer à leurs dispositions.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 15,
+                  fontFamily: "Monsera_Light",
+                ),
+              ),
+            ),
+            SizedBox(height: longueurPerCent(10.0, context),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("REFERENCE LEGALES",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 16,
+                  fontFamily: "MonseraBold",
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            SizedBox(height: longueurPerCent(10.0, context),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("Éditeur",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 16,
+                  fontFamily: "MonseraBold",
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("Raison sociale:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text("FOLLOW ME",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10,right: 10),
+                  child: Text("N° d’immatriculation:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text("RB/COT/19 A 51052 ",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: longueurPerCent(5.0, context),),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10,right: 10),
+                  child: Text("Siège sociale:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text("Ilot 1140, VODJE (Bénin),",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: longueurPerCent(5.0, context),),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10,right: 10),
+                  child: Text("E-mail:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text("followmebenin@gmail.com,",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: longueurPerCent(10.0, context),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("Développeur",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: HexColor("#001C36"),
+                  fontSize: 16,
+                  fontFamily: "MonseraBold",
+                ),
+              ),
+            ),
+            SizedBox(height: longueurPerCent(5.0, context),),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10,right: 10,bottom: 40),
+                  child: Text("Développeurs:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Text("SAVY Kévin, HOUEGBELO Amour",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: HexColor("#001C36"),
+                      fontSize: 16,
+                      fontFamily: "MonseraRegular",
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
